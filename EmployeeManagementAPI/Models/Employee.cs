877aea894efe331b8dfeb945d3456facb1760ee9
hamzatlh <htlhaouia@gmail.com>
2024-07-09 19:50:09 +0100

    add controller

--- EmployeeManagementAPI/Models/Employee.cs
+++ EmployeeManagementAPI/Models/Employee.cs
@@ -8,11 +8,11 @@ namespace EmployeeManagementAPI.Models
     public class Employee
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Position { get; set; }
-        public string Department { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Position { get; set; }
+        public string? Department { get; set; }
         }
 }
