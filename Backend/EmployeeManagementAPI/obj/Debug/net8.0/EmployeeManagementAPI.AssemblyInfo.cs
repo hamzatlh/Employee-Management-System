2074551d6f90795616ed70a49f4c2e291b9049c8
hamzatlh <htlhaouia@gmail.com>
2024-07-14 01:12:51 +0100

    fix link back front

--- Backend/EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ Backend/EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9cfb0fad08af5dafb37e6de6a6ae3c6e575050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22acca4dad50176ea32853522ac11070e02bf2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
