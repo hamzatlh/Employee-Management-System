2c1df764bc1289e7aece4e4fee856dcf44bbf36a
hamzatlh <htlhaouia@gmail.com>
2024-07-14 20:49:04 +0100

    starting styling

--- Backend/EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ Backend/EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3f9c983449ad1a30b43a3bdc5ba10ba5dec4c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e66abae3a0920862911b33a814f47d947522c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
